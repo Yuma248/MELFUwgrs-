5e1f82ab6de55f9eec9ea19f566fdd1c349313db
Yuma <yuma248@gmail.com>
2023-08-02 18:48:27 +0930

    Update NGSadmixP.pm

--- MELFUwgrs/NGSadmixP.pm
+++ MELFUwgrs/NGSadmixP.pm
@@ -1,15 +1,18 @@
 #!/usr/bin/perl -w
+package NGSadmix;
 
-while (@ARGV){
-	$_=shift @ARGV;
-	if ($_=~ /^-i$/){$input=shift @ARGV;}
-	elsif($_=~ /^-o$/){$output=shift @ARGV;}
-	elsif ($_=~ /^-mK$/){$mK=shift @ARGV;}
-	elsif ($_=~ /^-rep$/){$rep=shift @ARGV;}
-	elsif ($_=~ /^-ind$/){$ind=shift @ARGV;}  
-	elsif ($_=~ /^-nc$/){$nc=shift @ARGV;}
-	elsif ($_=~ /^-snc$/){$snc=shift @ARGV;}
-	elsif ($_=~ /^-maf$/){$maf=shift @ARGV;}
+my $LEVEL = 1;
+sub NGSadmixP{
+my @arg = @_;
+foreach $ar (@arg){
+	if  ($ar =~ /^-i/){our $input = (split(/ /,$ar))[1];}
+        elsif($ar=~ /^-o/){our $output = (split(/ /, $ar))[1];}
+        elsif ($ar=~ /^-nc/){$nc=(split(/ /, $ar))[1];}
+        elsif ($ar=~ /^-snc/){$snc=(split(/ /, $ar))[1];}
+	elsif ($ar=~ /^mK$/){$mK=(split(/ /, $ar))[1];}
+	elsif ($ar=~ /^-rep$/){$rep=(split(/ /, $ar))[1];}
+ 	elsif ($ar=~ /^-ind$/){$ind=(split(/ /, $ar))[1];}
+	elsif ($ar=~ /^-maf$/){$maf=(split(/ /, $ar))[1];}
 }
 
 if (not defined ($input )){print "\nUsage:\nNGSadmixP -i <beagle format input file>\nOptional:\n\t-o <output folder to save resutls, default ./NGSadmixR>\n\t-ind <minimum number of informative individuals, default 0>\n\t-mK <maximum K, number of populations assumed, default 5>\n\t-nc <number cores to run in parallel, default 2 >\n\t-snc <number of cores per each run, default 30>\n\t-rep <number replicates, default 1>\n\t-maf <minimum minor allele frequency, default 0.01>\n\nFor example:\n NGSadmixP -i yuma.str -o ./NGSadmixR/ -mK 10 -rep 5 -nc 3 -snc 30 -maf 0.05 \n\n"; exit;} 
@@ -28,3 +31,5 @@ our @Rs = (1..$rep);
 
 `parallel -j $nc NGSadmix -likes $input -minMaf $maf -K {1} -o $output\/NGSadmix_$ind\_{1}_{2} -minInd $ind -P $snc ::: @Ks ::: @Rs `;
 
+}
+1;
