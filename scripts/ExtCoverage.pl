697d3011465ea3f7f9338ae2d1b608bc12185440
Yuma <yuma248@gmail.com>
2025-04-14 16:36:38 +0930

    Create ExtCoverage.pl

---
+++ scripts/ExtCoverage.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use File::Path qw( make_path );
+while (@ARGV){
+	$_=shift @ARGV;
+	if ($_=~ /^-i$/){$input=shift @ARGV;}
+	elsif($_=~ /^-o$/){$output=shift @ARGV;}
+	elsif($_=~ /^-lnc$/){$lnc=shift @ARGV;}
+	elsif($_=~ /^-cl$/){$cl=shift @ARGV;}
+	elsif($_=~ /^-cn$/){$cn=shift @ARGV;}
+
+}
+if (not defined ($input)){print "\nThis pipeline calculate the depth coverage per site per sample. The required arguments are a input folder with sam or bam files.\n\nUsage:\nExtCoverage.pl\n\t-i <path to input folder with sam or bam files>\n\t-o <path to ouput folder to save results>\n\t-lnc <runs in parallel, defaul 10>\n\t-cl <list with scaffolds to consider, recomnet the largest and the closest to chromosome level>\n\t-cn <number of scaffolds or chromosome to consider if -cl is not defined, defaul 24>\n\nExample:\nExtDepth.pl -i /my/mapped/samples/ -o /my/mapped/depth/ -lnc 60 -cn 24"; exit;}
+$output //= "./coverage/";
+$lnc //= 10;
+$cn //= 24;
+if ( !-d $output) {
+        make_path $output or die "Failed to create path: $output";
+}
+opendir(DIR, "$input") or die "Can not open folder, $!\n" ;
+my @files = readdir(DIR);
+closedir (DIR);
+our @names=();
+foreach $file (@files){next unless $file =~ /.bam$/; chomp $file; $name=$file; $name=~ s/.bam$//g; push (@nms, $name);}
+if (defined ($cl)){$cn = `wc -l $cl | awk '{print \$1}'`};
+$cn1 = $cn + 1;
+
+my $cmd="parallel -j $lnc samtools coverage $input\/{1}.bam -o $output\/{1}.coverage ::: @nms";
+#print "$cmd\n\n";
+`parallel -j $lnc samtools coverage $input\/{1}.bam -o $output\/{1}.coverage ::: @nms`;
+
+if (not defined ($cl)){
+	`for i in \$(ls $output\/*.coverage); do samp=\$(basename "\$i"); echo -n \$samp >> $output/AverageCoverage; awk 'BEGIN{sum=0; chr=0} {if (NR>1 && NR<$cn) {sum += \$7; chr++}} END{print "\t"sum/chr}' \$i >> $output\/AverageCoverage; done`;
+	`plot_coverage.R $output $cn`;
+}
+else {
+	our $chrdir=$output."/CHR";
+	if ( !-d $chrdir ) {`mkdir $chrdir`;}
+	`for i in \$(ls $output\/*coverage); do samp=\$(basename "\$i"); cat $cl | while read l; do awk -v sca=\$l '\$1==sca' \$i >> $chrdir\/\$samp; done; done`;
+	`for i in \$(ls $chrdir\/*.coverage); do samp=\$(basename "\$i"); echo -n \$samp >> $chrdir\/AverageCoverage; awk 'BEGIN{sum=0; chr=0} {if (NR>1) {sum += \$7; chr++}} END{print "\t"sum/chr}' \$i >> $chrdir\/AverageCoverage; done`;
+	`plot_coverage.R $chrdir $cn`;
